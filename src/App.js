4e3bce1bef6ff7ecd585460344f7f2030bce8a16
green tea <dimmie@windowslaptop>
2019-10-31 13:18:39 +1100

    clean

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
 
   const [japaneseRelated, setJapaneseRelated] = React.useState(false);
 
-
   const [sl, setSl] = React.useState(0);
 
   const [tp, setTP] = React.useState(0);
@@ -21,7 +20,6 @@ function App() {
   const [entryPrice, setEntryPrice] = React.useState(0);
 
   const [standardLots, setStandardLots] = React.useState(0);
-
 
   const [atr, setAtr] = React.useState(0);
 
@@ -33,55 +31,33 @@ function App() {
   }
 
   const calculateBalance = (amount) => {
-
     setBalance(amount.target.value);
-
   }
 
   const changeAtr = (atr) => {
-    debugger;
-
     setAtr(Number(atr.target.value))
-
-
   }
 
   const changeJapaneseRelated = (chb) => {
-
-
-
     if (chb.target.checked) {
       setJapaneseRelated(true)
-
     }
-
     else {
       setJapaneseRelated(false)
     }
-
 
   }
 
 
   const calculateRisk = (riskPercentage) => {
-
-
     let tmp = Number(riskPercentage.target.value) * 0.01 * balance;
-
     setAmountRisked(tmp);
-
-
-
   }
 
 
-
   const changeStandardLots = () => {
-
     let temp = amountRisked / sl;
-
     standardLots = temp * 10 //this is a microlot $5000 balance to start
-
   }
 
 
@@ -92,11 +68,11 @@ function App() {
     let price = Number(entryPrice);
 
     //atr is pips so convert to decimals:
-    let sl_multiply_factor = 1;
-    let atr_multiplied =  japaneseRelated ? atr*0.01 * sl_multiply_factor: atr*0.0001*sl_multiply_factor;
+    let sl_multiply_factor = 3;
+    let atr_multiplied = japaneseRelated ? atr * 0.01 * sl_multiply_factor : atr * 0.0001 * sl_multiply_factor;
 
-    let tp_multiply_factor =0.75;
-    let atr_tp =  japaneseRelated ? atr*0.01*tp_multiply_factor: atr*0.0001*tp_multiply_factor;
+    let tp_multiply_factor = 1;
+    let atr_tp = japaneseRelated ? atr * 0.01 * tp_multiply_factor : atr * 0.0001 * tp_multiply_factor;
 
     //SET SL
     if (longPosition) {
@@ -141,25 +117,13 @@ function App() {
       }
     }
 
-    // e.stopPropagation();
+
   }
 
   const changeEntryPrice = (entry) => {
     setEntryPrice(entry.target.value);
 
   }
-
-  // const change_atr = (val) =>{
-  //   if (japaneseRelated)
-  //   {
-  //     setAtr(Numbe
-  //   }
-  //   else
-  //   {
-  //     setAtr(atr.target.value)
-  //   }
-
-  // }
 
 
   const changeLongPosition = (chb) => {
@@ -183,32 +147,19 @@ function App() {
 
 
 
+        {' '}<input type="checkbox" onChange={changeJapaneseRelated} />{' '}{japaneseRelated ? 'This pair is JPY related' : 'This not JPY related.'}
+        <br />
         {' '}<input type="checkbox" onChange={changeLongPosition} />{' '}{longPosition ? 'BUY' : 'SELL'}
         <br />
         <input type="number" placeholder="ATR in pips" onChange={changeAtr} />
-        <br/>
-
-        {' '}<input type="checkbox" onChange={changeJapaneseRelated} />{' '}{japaneseRelated ? 'JPY enabled' : ''}
         <br />
-        {/* <input type="number" placeholder="balance" onChange={calculateBalance} /> */}
-        {/* <input type="number" placeholder="risk percentage" onChange={calculateRisk} /> */}
-
-
-     
-
-
-        {/* <input type="number" placeholder="sl" value={sl} onChange={calculate} /> */}
 
         <input type="number" placeholder="Entry price" onChange={changeEntryPrice} />
 
 
-        <br/>
+        <br />
         <input type="button" onClick={calculateSL_TP} value="Calculate SL and TP"></input>
-        <br/>
-        {/* <label>amountRisk:{' '}$</label>
-        {amountRisked}<br /> */}
-        {/* <label>position size(lots):{' '}</label> */}
-        {/* {standardLots}<br /> */}
+        <br />
 
 
         <label>SL:</label>
@@ -223,18 +174,6 @@ function App() {
         <input type="number" value={tp}></input>
         <br />
 
-
-        {/* <input type="button" onClick={change_atr} value="STEP 1 : calculate ATR"></input><br/> */}
-    
-
-
-
-
-
-        {/* <input type="number" ref="principal" placeholder="Loan Amount" value={this.state.value.principal} onChange={this.handleChange}/><br/>
-      <input type="number" ref="rate" placeholder="Interest Rate" value={this.state.value.rate} onChange={this.handleChange}/><br/>
-      <input type="number" ref="term" placeholder="Length of loan" value={this.state.value.term} onChange={this.handleChange}/><br/>
-      <h3>Your estimated monthly payment is ${this.state.payment}</h3> */}
       </form>
     </div>
   );
